18f543b58392d54ace5258608609cab7b01d421b
Omer <OZadok@gmail.com>
2018-01-25 23:10:32 +0200

    1

--- TranZmission/Assets/Player/PlayerBehavior.cs
+++ TranZmission/Assets/Player/PlayerBehavior.cs
@@ -11,11 +11,21 @@ public class PlayerBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        movement();
+
+    }
 
     private void movement()
     {
-
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h != 0 | v != 0)
+        {
+            float direction = Mathf.Atan2(v, h);
+            float dx = Mathf.Cos(direction) / 100;
+            float dy = Mathf.Sin(direction) / 100;
+            this.transform.position = this.transform.position + new Vector3(dx, dy, 0);
+        }
+        
     }
 }
